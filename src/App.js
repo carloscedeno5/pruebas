a17583b6f8b030f59a0f59d8e9d4517a7ef8bd5c
cedeno <ccedeno@ucol.mx>
2018-07-16 17:29:51 -0500

    Practicando las props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// function Hello (props){
+//   return <h2>{props.title}</h2>
+// }
+
+// const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component {
+  render () {
+    return <h2>{this.props.title}</h2>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenidos a este curso</h1>
+          <Hello title='Hello from props'/>
         </header>
         <h3>Estamos aprendiendo react</h3>
         <p className="App-intro">

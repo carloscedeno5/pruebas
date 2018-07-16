3d50e2123a8ad33472d2d0001f5ba0bcfc08b076
cedeno <ccedeno@ucol.mx>
2018-07-16 15:14:35 -0500

    agregando ejercicio uno del curso

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Bienvenidos a este curso</h1>
+        </header>
+        <h3>Estamos aprendiendo react</h3>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
